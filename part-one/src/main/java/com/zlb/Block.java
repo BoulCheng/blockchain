f8b2e3097641d785f393f4bddd9ce73fef8095d7
BoulCheng <anyangleguan25>
2018-04-17 14:28:50 +0800

    挖矿

--- part-one/src/main/java/com/zlb/Block.java
+++ part-one/src/main/java/com/zlb/Block.java
@@ -14,6 +14,7 @@ public class Block {
     public String previousHash;
     private String data; //our data will be a simple message.
     private long timeStamp; //as number of milliseconds since 1/1/1970.
+    private int nonce;//随机数
 
     //Block Constructor.
     public Block(String data,String previousHash) {
@@ -27,8 +28,26 @@ public class Block {
         String calculatedhash = StringUtil.applySha256(
                 previousHash +
                         Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
                         data
         );
         return calculatedhash;
+    }
+
+    /**
+     * mineBlock()方法中引入了一个int值称为difficulty难度，低的难度比如1和2，
+     * 普通的电脑基本都可以马上计算出来，我的建议是在4-6之间进行测试，普通电脑大概会花费3秒时间，
+     * 在莱特币中难度大概围绕在442592左右，而在比特币中每一次挖矿都要求大概在10分钟左右，
+     * 当然根据所有网络中的计算能力，难度也会不断的进行修改
+     * @param difficulty
+     */
+    public void mineBlock(int difficulty) {
+        //Create a string with difficulty * "0"
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring(0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 }
